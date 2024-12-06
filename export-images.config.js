56b66bdfd326edf9ab5cd9957974800b923fd5fc
le-nn <le.dev.nn@outlook.com>
2024-12-06 16:11:09 +0900

    リンク切れ修正

--- export-images.config.js
+++ export-images.config.js
@@ -2,11 +2,13 @@
  * @type {import('next-export-optimize-images').Config}
  */
 const config = {
-    basePath: "/match-bond-new",
+    basePath: "match-bond-new",
     convertFormat: [
         ['png', 'webp'],
         ['jpg', 'webp'],
     ],
 }
+
+console.log('------------------------------------------------------', config)
 
 module.exports = config
--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,6 @@ import withExportImages from 'next-export-optimize-images'
 const nextConfig = {
     output: 'export',
     basePath: "/match-bond-new",
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/views/api-reference/components/image#unoptimized
-     */
     images: {
     },
     compiler: {
