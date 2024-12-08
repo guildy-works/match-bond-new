6986ea4853b135bf8521865bdcd53726e418dd51
le-nn <le.dev.nn@outlook.com>
2024-12-08 13:34:51 +0900

    ビルドエラー修正

--- src/components/HighlightMarker.tsx
+++ src/components/HighlightMarker.tsx
@@ -8,7 +8,7 @@ export const HighlightMarker = ({ children, }: {
         <ScrollTrigger className={`relative inline-block overflow-hidden mx-1 -mb-1`} once>{
             state => <span>
                 <span className={`relative z-10 font-bold`}>{children}</span>
-                <FadeAndSlideAnimation transform={{ translate: { x: "-200px" } }} in={state == 'entered'}
+                <FadeAndSlideAnimation transform={{ translate: { x: "-200px" } }} in={state.triggered}
                     className={`absolute left-0 bottom-0 w-full h-1/2 bg-color3 opacity-50 transform -rotate-2`}
                     style={{
                         content: '""',
